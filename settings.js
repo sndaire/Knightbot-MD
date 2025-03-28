e528b9000169bbec69d2388806f997238eccc2c0
Samuel Ndaire Njoroge <96482879+sndaire@users.noreply.github.com>
2025-03-28 13:51:09 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'NDA1R3', // Your name
+  ownerNumber: '254727067368', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
