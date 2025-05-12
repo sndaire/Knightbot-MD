66d13277b6c640abb25a41ed8b3175abaa30975f
Samuel Ndaire Njoroge <96482879+sndaire@users.noreply.github.com>
2025-05-12 14:03:46 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Unity Bot',
   author: '‎',
   botName: "Unit Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'Ndaire', // Your name
   ownerNumber: '254727067368', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
