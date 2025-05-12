7460ec6f6f618b3adbf3d606f29c8019fd0a29be
Samuel Ndaire Njoroge <96482879+sndaire@users.noreply.github.com>
2025-05-12 14:03:14 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Unity Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Unit Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '254727067368', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.0",
 };
