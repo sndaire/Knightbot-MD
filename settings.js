b80e7903cee13e4af472bf53547f9cdf4dcbf7ef
Samuel Ndaire Njoroge <96482879+sndaire@users.noreply.github.com>
2025-05-21 15:07:21 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   botOwner: 'Ndaire', // Your name
   ownerNumber: '254727067368', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "private",
+  commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.1",
 };
